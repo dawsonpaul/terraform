48610890b03afb7e0d88869772568f08d08e5b28
Paul Dawson <dawsonpaul@gmail.com>
2021-07-16 22:26:00 +0100

    state lock output

---
+++ remote_storage/terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "pd-lab-terraformbucket"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "pd-lab-dynamodb-lock"
+    encrypt        = true
+  }
+}
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
+}
