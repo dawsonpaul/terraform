0d5c7df0735eeb228f416a27743185552f0d2229
Paul Dawson <dawsonpaul@gmail.com>
2021-07-20 11:54:23 +0100

    updated s3 key dir

--- prod/data-stores/mysql/backend.tf
+++ prod/data-stores/mysql/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     # Replace this with your bucket name!
     bucket         = "pd-lab-terraformbucket"
-    key            = "stage/data-stores/mysql/terraform.tfstate"
+    key            = "prod/data-stores/mysql/terraform.tfstate"
     region         = "us-east-2"
 
     # Replace this with your DynamoDB table name!
